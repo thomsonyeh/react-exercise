6613e64d87acd90cc6384b5b75bdbf1906fd81b1
thomson <thomsonyeh@cmu.edu>
2017-07-29 16:56:05 -0700

    first commit

---
+++ candidates.js
@@ -0,0 +1,63 @@
+var candidates =
+[
+    {
+        "id": 1,
+        "name": "Kathern Machado",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 2,
+        "name": "Nick Wenzel",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 3,
+        "name": "Brittani Sobus",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 4,
+        "name": "Donetta Brubaker",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 5,
+        "name": "Coleman Harbert",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 6,
+        "name": "Antoine Byerley",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 7,
+        "name": "Penney Mccomas",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 8,
+        "name": "Lakeshia Callaham",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 9,
+        "name": "Efrain Dower",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"},
+    {
+        "id": 10,
+        "name": "Porsche Marko",
+        "status": "needs screening",
+        "applicationDate": "5/1/2017",
+        "location": "new york"}
+    ]
